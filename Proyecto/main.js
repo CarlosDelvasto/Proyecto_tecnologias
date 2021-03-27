690a0b95003a9f4b7c43ea9cdae6bef08271ad39
CarlosDelvasto <est.carlos.delvasto@unimilitar.edu.co>
2021-03-26 21:38:35 -0500

    Sección Inicio (Sin terminar)
    Agregue botones de inicio de sesión y abrí las secciones para cada una y agregue cambio de pestañas para esas secciones, tiene responsive con bootstrap

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -9,8 +9,23 @@ $(document).ready(function(){
         $('.secciones article').hide();
 
         var activeTab = $(this).attr('href');
+        $('.secciones section').hide(); 
         $(activeTab).show();
         return false;
+
+
     });
 });
+$(document).ready(function(){
+    
+    $('.secciones section').hide();
+    $('ul.btn li a').click(function(){
+        $('.secciones section').hide();
 
+        var activeBtn = $(this).attr('href');
+        $('.secciones article').hide();
+        $(activeBtn).show();
+        return false;
+
+    });
+});
