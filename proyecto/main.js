67d837e43b202585204cabf18e4ca1066109229c
JulietaRooz <est.julieta.rodrig@unimilitar.edu.co>
2021-04-02 20:58:52 -0500

    Añadí cosas en multimedia
    Añadí la estructura del modelo 3D, el logo y parte del codigo de la union de los links (aunque no sirvio)

--- proyecto/main.js
+++ proyecto/main.js
@@ -29,3 +29,63 @@ $(document).ready(function(){
 
     });
 });
+
+
+
+$(document).ready(function(){
+    $('Panel_InicioSesion').show();
+    $('form.p #1a').click(function(){
+        $('.Panel_InicioSesion').hide();
+
+        var activeBtn = $(this).attr('href');
+        $('.Panel_InicioSesion').hide();
+        $(Panel_Registro).show();
+        return false;
+
+    });
+});
+
+
+
+$(document).ready(function(){
+    $('Panel_Registro').show();
+    $('form.p #2a').click(function(){
+        $('.Panel_Registro').hide();
+
+        var activeBtn = $(this).attr('href');
+        $('.Panel_Registro').hide();
+        $(Panel_InicioSesion).show();
+        return false;
+
+    });
+});
+
+
+
+
+window.onload = iniciar;
+function iniciar()
+{
+document.getElementById ("enviar").addEventListener('click',validar,false);
+
+}
+
+function validaIniciosesion(){
+    var element = document.getElementsByClassName ("controls");
+    if (MediaElementAudioSourceNode.value == "")
+    {
+        alert("El campo no puede estar vacío");
+        return false;
+    }
+    return true;
+}
+
+function validaRegistro(){
+    var element = document.getElementsByClassName ("controlsr");
+    if (MediaElementAudioSourceNode.value == "")
+    {
+        alert("El campo no puede estar vacío");
+        return false;
+    }
+    return true;
+}
